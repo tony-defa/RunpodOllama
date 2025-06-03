1e04980c673614d71916dfc5ba9717e8c1f03fb4
Antonio De Falco <antonio.defalco@irisnet.de>
2025-06-03 14:05:46 +0200

    removed stuff

--- runpod_ollama/utils.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-
-def is_port_free(port: int) -> bool:
-    """
-    Check if a port is free
-    """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(("localhost", port)) != 0
